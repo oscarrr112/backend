2379b6129176fde737f75301cab1c1bea75d64c3
离猝死不远队 <1261223497@qq.com>
2020-03-17 23:26:57 +0800

    第一次上传

--- myapp/userviews.py
+++ myapp/userviews.py
@@ -16,6 +16,7 @@ URL = 'http://127.0.0.1:8000/media/'
 def register(request):
     response = {}
     try:
+        print(request.FILES)
         json_result = json.loads(request.FILES.get('data'))
         try:
             try:
